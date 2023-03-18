381fcdf5f97b03c807e6213361db54ac25a5b21b
schmijul <julian.schmidt2@mailbox.tu-dresden.de>
2023-03-18 18:10:57 +0100

    added more dynamic fibonacci allg.

--- fib.py
+++ fib.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python
 
-
-
-
-"""
-Task:
-        Write a funciton 'fib(n)' that takes in a number as an argument.
-        The function should return the -th number of the Fibonnaci Sequenz
-        The first and second number of the sequence is 1.
-                    
-        To generate the net number of the sequence one must sum the prevoius two
-"""
-
-
-
+import numpy as np
 
 
 
@@ -28,9 +15,26 @@ def simple_fib(n):
     """
     This function is a simple Fibonacci implementation using recursion.
     It will wrk very quick for a low n, however if n is very high the number of calculations is so high that the programm will become ve    ry slow
+
+
+    This implementation is O(n) in both space and time complexity
     """
-    if n <= 1: return n 
-    return simple_fib(n - 1) + simple_fib(n - 2) 
+    if (n <= 1): return n 
+    return simple_fib(n - 1) + simple_fib(n - 2)
+
+
+
+
+
+
+@vs(node_properties_kwargs={"shape":"record", "color":"#f57542", "style":"filled", "fillcolor":"grey"})
+def dynamic_fib(n):
+    a = [0] * (n + 1)
+    a[0] = 0
+    a[1] = 1
+    for i in range(2, n + 1):
+        a[i] = a[i-1] + a[i-2]
+    return a[n]
 
 
 def main():
